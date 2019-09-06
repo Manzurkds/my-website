c255d7de643abf94e21886aff797d1b491a38e01
“manzurkds” <manzurkds@gmail.com>
2019-09-06 20:57:16 +0530

    feat: Tailwind configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      ... colors,
+      background: 'var(--background-color)',
+      primary: 'var(--primary-color)',
+      secondary: 'var(--secondary-color)',
+      fade: 'var(--fade-color)',
+      accent: 'var(--accent-color)',
+    }
   },
   variants: {},
   plugins: []
