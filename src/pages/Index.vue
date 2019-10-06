e2d47ff4377a789316bbe5e88c82c6ee09ded30f
“manzurkds” <manzurkds@gmail.com>
2019-10-06 12:00:13 +0530

    feat: Added social and profession profile links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,6 +15,36 @@
         <g-image class="w-80 mt-8 sm:w-64" alt="Manzur Khan" src="~/assets/images/manzur.jpg" />
       </aside>
     </section>
+    <section class="mt-20">
+      <h1 class="text-secondary">Find me on:</h1>
+      <div class="flex mt-5">
+        <a class="flex items-center mr-4"
+          href="https://linkedin.com/in/manzurkds" target="_blank">
+          <g-image class="w-8 mr-1" alt="LinkedIn logo" src="~/assets/icons/linkedin.png" />
+          LinkedIn
+        </a>
+        <a class="flex items-center mr-4"
+          href="https://github.com/manzurkds" target="_blank">
+          <g-image class="w-8 mr-1" alt="Github logo" src="~/assets/icons/github.png" />
+          GitHub
+        </a>
+        <a class="flex items-center mr-4"
+          href="https://stackoverflow.com/users/6883002/manzur-khan" target="_blank">
+          <g-image class="w-8 mr-1" alt="Stackoverflow logo" src="~/assets/icons/stackoverflow.png" />
+          Stackoverflow
+        </a>
+        <a class="flex items-center mr-4"
+          href="https://facebook.com/manzurkds" target="_blank">
+          <g-image class="w-8 mr-1" alt="Facebook logo" src="~/assets/icons/facebook.png" />
+          Facebook
+        </a>
+        <a class="flex items-center mr-4"
+          href="https://twitter.com/manzurkds" target="_blank">
+          <g-image class="w-8 mr-1" alt="Twitter logo" src="~/assets/icons/twitter.png" />
+          Twitter
+        </a>
+      </div>
+    </section>
 
     <!-- <p class="home-links">
       <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
